e4e695ae7fe6bb1f5721362eb78483b92ed45068
opmaksim <tjckdals330@naver.com>
2024-10-11 16:13:57 +0900

    RC car

---
+++ car/Driver/Motor/Motor.h
@@ -0,0 +1,12 @@
+#ifndef MOTOR_H_
+#define MOTOR_H_
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+#include "../../Periph/GPIO/GPIO.h"
+#define MOTOR_ICR		ICR1
+#define MOTOR_OCR		OCR1A
+
+void Motor_init();
+void Motor_speedMode(uint16_t data);
+#endif
---
+++ car/Periph/TIM/TIM.c
@@ -0,0 +1,19 @@
+#include "TIM.h"
+
+
+void TIM0_init(){
+    TCCR0B |= ((0<<CS02) | (1<<CS01) | (1<<CS00));
+    //CTC Mode
+    TCCR0B |= ((0<<WGM02));
+    TCCR0A |= ((1<<WGM01) | (0<<WGM00));
+    TCCR0A &= ~((1<<COM0A1) | (1<<COM0A0));
+    OCR0A = 250-1;
+    TIMSK0 |= (1 << OCIE0A);
+}
+
+void TIM1_init(){
+    TCCR1A |= ((1<<WGM11) | (0<<WGM10)); 
+    TCCR1B |= ((1<<WGM13) | (1<<WGM12));
+    TCCR1B |= ((0<<CS12) | (1<<CS11) | (1<<CS10));
+    TCCR1A |= (1<<COM1A1) | (0<<COM1A0);
+}
